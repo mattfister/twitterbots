c52a28d570e4748aa102a94d7ad8fd3230d2770c
mattfister@gmail.com <mattfister@gmail.com>
2014-11-02 12:14:06 -0500

    Rearchitected. Added replies

--- connectorV1.js
+++ connectorV1.js
@@ -1,7 +1,7 @@
 // This is a twitter bot that attempts to connect two people talking about the same trend.
 // Every 15 minutes it picks a trend, announces what it's thinking about.
-// Then it listens for 100 tweets about that trend. Once this is done it uses those
-// 100 tweets to construct a new tweet via a Markov chain of those inputs. It tweets this.
+// Then it listens for 5 minutes of tweets about that trend. Once this is done it uses those
+// tweets to construct a new tweet via a Markov chain of those inputs. It tweets this.
 // Then it picks two of the people who it had listened to and tries to connect them to each other
 // The bot wins if those two people start talking.
 
@@ -131,20 +131,29 @@ var choice = function (a) {
 // Assuming our corpus has been generated, creates a tweet of at least size minLength
 // and no bigger than 140 characters
 var markovTweet = function(minLength) {
-    word = choice(startwords);
-    var title = [word];
-    console.log("startWord: " + word);
-    while (wordstats.hasOwnProperty(word)) {
-        var next_words = wordstats[word];
-	console.log("nextWords: " + next_words);
-        word = choice(next_words);
-	console.log("nextWord: " + word);
-        title.push(word);
-        if (title.length > minLength && terminals.hasOwnProperty(word)) break;
+    var goodTweet = false;
+    var tweet = [""];
+    while (goodTweet == false ) {
+	word = choice(startwords);
+	tweet = [word];
+	console.log("startWord: " + word);
+	while (wordstats.hasOwnProperty(word)) {
+            var next_words = wordstats[word];
+	    console.log("nextWords: " + next_words);
+            word = choice(next_words);
+	    console.log("nextWord: " + word);
+            tweet.push(word);
+            if (terminals.hasOwnProperty(word)) {
+		break;
+	    }
+	}
+	tweet = tweet.join(' ');
+	if (tweet.length < 140) {
+	    goodTweet = true;
+	}
     }
-    if (title.length < minLength || title.length > 140) return markovTweet(minLength);
-    return title.join(' ');
-};
+    return tweet;
+}
 
 // Chooses two random distinct users from all the tweets we listened to.
 // These are the ones that we're going to try to connect
@@ -165,26 +174,67 @@ function followUsers() {
     });
 }
 
+var thoughts = ["Thinking about ", "Considering ", "Wondering about ", "Contemplating ", "Learning about ", "Interested in ", "Been thinking about ", "Feeling like "];
+
 function postAboutTopic(topic) {
-    var generalPost = "Thinking about " + topic;
-    postTweet(generalPost);
+    var generalPost = choice(thoughts) + topic;
+    console.log("\n\n");
+    console.log("|||||||||||| Post: " + generalPost);
+    console.log("\n");
+    //postTweet(generalPost);
 }
 
 // Keep track of if we're already processing tweets, so we don't start again
 var processing = false;
 
-// Listen for this many tweets before we start posting
-var numToListenTo = 100;
+var numAdded = 0;
+var stream;
+var startListeningTime = null;
 
-// This guy does all the magic. Picks a trend in the US, listens to a bunch of tweets about it,
-// generates a new tweet about it based on a Markov chain of all the tweets we listened to,
-// follows two of the people who tweeted about it, then tries to connect them (phew!)
-function connectorPost() {
+// This keeps track of how our streamin is doing
+// It will make decisions on whether we should stop or continue
+// and if it's been going well enough for 5 minutes, it will
+// send a tweet.
+function monitorStreaming() {
+    if (processing == true && startListeningTime != null) {
+	var currentTime = Date.now();
+	var timePassed = currentTime - startListeningTime;
+	console.log("Been listening to " + topic + " for " + timePassed/1000.0 + "s. Added " + numAdded + " tweets");
+	if (timePassed > 60000 && numAdded < 10) {
+	    stream.stop();
+	    processing = false;
+	    console.log("Abandoning topic: " + topic);
+	} else if (timePassed > 60000*5) {
+	    var newStatus = markovTweet();
+	    console.log("\n|||||||||||| Status: " + newStatus);
+	    postTweet(newStatus);
+	    stream.stop();
+	    chooseUsers();
+	    followUsers();
+	    sendConnectTweet(user1, user2);
+	    processing = false;
+	}
+    } else {
+	console.log("Not currently streaming\n");
+    }
+}
+
+
+connections = [" do you know ", " you know ", " what's up with ", " who is ", " you seem kind of like "] 
+function sendConnectTweet(user1, user2) {
+    connectionStatus = "@" + user1 + choice(connections) + "@" + user2 + "?"; 
+    console.log("\n||||||||||| ConnectionStatus: " + connectionStatus);
+    postTweet(connectionStatus);
+}
+
+// This sets up our streaming. Picks a trend in the US, listens to a bunch of tweets about it,
+// and adds the tweets to our corpus
+function connectorStream() {
     if (processing == true ) {
 	console.log("Still processing...");
 	return;
     }
-
+    startListeningTime = null;
     processing = true;
     
     console.log("\n");
@@ -209,76 +259,112 @@ function connectorPost() {
 	postAboutTopic(topic);
 	
 	console.log("Starting listening");
-	var stream = T.stream('statuses/filter', { track: topic })
+	stream = T.stream('statuses/filter', { track: topic })
 	
-	var numAdded = 0;
-	var startListeningTime = Date.now();
+	startListeningTime = Date.now();
 	stream.on('tweet', function(tweet) {
-	    // console.log("screen_name: " + tweet.screen_name);
-	    var currentTime = Date.now();
-	    var timePassed = currentTime - startListeningTime;
-	    console.log("Been listening for " + timePassed/1000.0 + "s");
-	    if (timePassed > 60000*5 && numAdded < numToListenTo/3) { // If we've been listening for more than 5 minutes, and we don't have a third as many tweets as we need, bail
-		stream.stop();
-		processing = false;
-		console.log("Abandoning topic: " + topic);
-	    }
 	    if (passesFilter(tweet.text)) {
-		console.log("Adding tweet " + numAdded + " by user: " + tweet.user.screen_name);
-		console.log("text: " + tweet.text);
+		// console.log("Adding tweet " + numAdded + " by user: " + tweet.user.screen_name);
+		// console.log("text: " + tweet.text);
 		addTweet(tweet.text);
 		addUser(tweet.user.screen_name);
 		numAdded++;
-		console.log("\n");
-		if (numAdded >= numToListenTo) {
-		    var newStatus = markovTweet();
-		    console.log("Status: " + newStatus);
-		    postTweet(newStatus);
-		    stream.stop();
-		    chooseUsers();
-		    followUsers();
-		    var connectionStatus = "@" + user1 + " do you know @" + user2 + "?";
-		    
-		    console.log("ConnectionStatus: " + connectionStatus);
-		    postTweet(connectionStatus);
-
-		    processing = false;
-		}
 	    }
-	});	
+	});
     });
 }
 
-// Every 5 hours go through and favorite retweets
-function favRTs () {
-  T.get('statuses/retweets_of_me', {}, function (e,r) {
-    for(var i=0;i<r.length;i++) {
-      T.post('favorites/create/'+r[i].id_str,{},function(){});
+// Compares two long string integers
+// Used because js has problems with int64s
+var stringGreaterThan = function(a, b) {
+    // Pad string a to size of string b
+    while (a.length < b.length) {
+	a = "0" + a;
     }
-    console.log('harvested some RTs'); 
-  });
+    // Pad string b to size of string a
+    while (b.length < a.length) {
+	b = "0" = b;
+    }
+    
+    // now compare them
+    return a > b;
 }
 
-// Every 15 minutes try to start connecting people
-setInterval(function() {
+// Keep track of which users we applied to
+repliedToUsers = {};
+replyMessages = [" you both seem p cool", " thought you were both interesting", " :)", " you're cool", " you both seem chill", " lol"]
+function postReplyToMention(screenName, replyToId) {
+    if (!repliedToUsers.hasOwnProperty(screenName)) {
+	repliedToUsers[screenName] = true;
+	tweet = "@" + screenName + choice(replyMessages);
+	T.post('statuses/update', {status: tweet, in_reply_to_status_id: replyToId}, function(err, data, reply) {
+	});
+	console.log("||||||||| Replied: " + tweet);
+    } else {
+	console.log("Wanted to reply to screenName: " + screenName, ", but already replied to them");
+    }
+}
+
+
+var lastMentionIdStr = null;
+var firstMentionPull = true;
+function handleMentions() {
+    console.log("Getting mentions\n");
+    T.get('statuses/mentions_timeline', {}, function(e,r) {
+	for (var i=0; i < r.length; r++) {
+	    if (lastMentionIdStr == null || stringGreaterThan(r[i].id_str, lastMentionIdStr) ) {
+		lastMentionIdStr = r[i].id_str;
+		console.log("Setting last mentionId to " + lastMentionIdStr);
+		if (firstMentionPull == false) {
+		    console.log("\n!!!!!!!!! Got a new mention!");
+		    console.log("Mention from user: " + r[i].user.screen_name);
+		    console.log("Mention text: " + r[i].text);
+		    postReplyToMention(r[i].user.screen_name, r[i].id_str);
+		}
+	    }
+	}
+	firstMentionPull = false;
+    });
+}
+
+
+
+function streamers() {
     try {
-	connectorPost();
+	connectorStream();
     } catch (e) {
 	console.log(e);
     }
-}, 60000*15 );
+}
 
-// every 5 hours, check for people who have RTed me, and favorite them
+// Every 1 minute  try to start streaming
+setInterval(streamers, 60000*1 );
+
+// Every 15 seconds see how we're doing
 setInterval(function() {
-  try {
-    favRTs();
-  }
- catch (e) {
-    console.log(e);
-  }
-},60000*60*5);
+    try {
+	monitorStreaming();
+    } catch (e) {
+	console.log(e);
+    }
+}, 1000 * 15 ); 
 
 // Wait 2 seconds after startup and start posting
-setTimeout(function() {
-    connectorPost();
-}, 2000);
+setTimeout(streamers, 2000);
+
+function mentions() {
+    try {
+	handleMentions();
+    } catch (e) {
+	console.log(e);
+    }
+}
+
+// Every 10 minutes see if we have new mentions
+setInterval(mentions, 60000 * 10);
+
+// Get our first mentions 5 seconds after we start
+setTimeout(mentions, 5000);
+
+// Do a quick first pass on mentions just to test
+setTimeout(mentions, 60000 * 1);
