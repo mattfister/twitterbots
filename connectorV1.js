3cafe2c9ec9c258b392d3e29298f1109abf78424
mattfister@gmail.com <mattfister@gmail.com>
2014-11-03 22:17:42 -0500

    Fix error handling. Fix encoded html characters. Make markov function do some rough ranking

--- connectorV1.js
+++ connectorV1.js
@@ -51,6 +51,7 @@ setTimeout( function() {
 
 
 // These are all of our state variables to keep track of what we're doing
+var numAdded = 0; // How many tweets we've added
 var terminals = {}; // map of words:true/false if each word in our markov chains is true or false
 var startwords = []; // start word options for our markov chains
 var wordstats = {}; // a map of words to words that follow it
@@ -61,6 +62,7 @@ var trends = []; // the top 10 trends in the US
 
 // Resets our state (all the above variables)
 function resetCorpus() {
+    numAdded = 0;
     terminals = {};
     startwords = [];
     wordstats = {};
@@ -81,7 +83,7 @@ var chooseTrend = function() {
 	} else if (trend.indexOf("?") >= 0) {
 	    continue;
 	} else {
-	    return trend; 
+	    return trend;
 	}
 	
     }
@@ -89,17 +91,29 @@ var chooseTrend = function() {
 
 // Filters out tweets that start with RT, these result in us trying to generate RTs which is a fool's game
 var passesFilter = function(tweet) {
+    if (tweet.indexOf("trending") > -1) {
+	return false;
+    }
+    if (tweet.indexOf("1.") > -1 && tweet.indexOf("2.") > -1) {
+	return false;
+    }
     var words = tweet.split(' ');
     if (words[0].toUpperCase() == "RT") {
 	return false;
-    } else {
-	return true;
     }
+    return true;
 }
 
 // Adds a tweet to our Markov chain state, keeping track of how
 function addTweet(tweet) {
     var words = tweet.split(' ');
+    for (var i = 0; i < words.length; i++ ) {
+	words[i] = words[i].replace("\"", ""); // Strip out quotes around words - it's just confusing
+	words[i] = words[i].replace("&amp;", "&"); // Replace ampersands with ampersands
+	words[i] = words[i].replace("&gt;", ">"); // Replace greater thans with >
+	words[i] = words[i].replace("&lt;", "<"); // Replace less thans with <
+	words[i] = words[i].replace("&#39;", "'"); // Replace quotes with '
+    }
     terminals[words[words.length-1]] = true;
     startwords.push(words[0]);
     for (var j = 0; j < words.length - 1; j++) {
@@ -119,6 +133,9 @@ function addUser(user) {
 // Posts a tweet to our timeline
 function postTweet(tweet) {
     T.post('statuses/update', {status: tweet}, function(err, data, reply) {
+	if (err) {
+	    return handleError(err);
+	}
     });
 }
 
@@ -129,30 +146,43 @@ var choice = function (a) {
 };
 
 // Assuming our corpus has been generated, creates a tweet of at least size minLength
-// and no bigger than 140 characters
+// and no bigger than 140 characters. Generate 10 of these and take the best defined as
+// the sum of the number of words that follow each word
 var markovTweet = function(minLength) {
-    var goodTweet = false;
-    var tweet = [""];
-    while (goodTweet == false ) {
-	word = choice(startwords);
-	tweet = [word];
-	console.log("startWord: " + word);
-	while (wordstats.hasOwnProperty(word)) {
-            var next_words = wordstats[word];
-	    console.log("nextWords: " + next_words);
-            word = choice(next_words);
-	    console.log("nextWord: " + word);
-            tweet.push(word);
-            if (terminals.hasOwnProperty(word)) {
-		break;
+    // rank these tweets
+    var maxRank = 0;
+    var bestTweet = "";
+    for (var i = 0; i < 10; i++ ) {
+	var goodTweet = false;
+	while (goodTweet == false ) {
+	    var curRank = 0;
+	    var tweet = [""];
+	    word = choice(startwords);
+	    tweet = [word];
+	    console.log("startWord: " + word);
+	    while (wordstats.hasOwnProperty(word)) {
+		var next_words = wordstats[word];
+		curRank += next_words.length;
+		// console.log("nextWords: " + next_words);
+		word = choice(next_words);
+		// console.log("nextWord: " + word);
+		tweet.push(word);
+		if (terminals.hasOwnProperty(word)) {
+		    break;
+		}
+	    }
+	    tweet = tweet.join(' ');
+	    if (tweet.length < 140) {
+		goodTweet = true;
+		if (curRank > maxRank) {
+		    maxRank = curRank;
+		    bestTweet = tweet;
+		    console.log(i + "\tRank: " + maxRank + " Tweet: " + bestTweet);
+		}
 	    }
 	}
-	tweet = tweet.join(' ');
-	if (tweet.length < 140) {
-	    goodTweet = true;
-	}
     }
-    return tweet;
+    return bestTweet;
 }
 
 // Chooses two random distinct users from all the tweets we listened to.
@@ -169,8 +199,14 @@ function chooseUsers() {
 // Follow our two special users
 function followUsers() {
     T.post('friendships/create', { id: user1 }, function(err, data, reply) {
+	if (err) {
+	    return handleError(err);
+	}
     });
     T.post('friendships/create', { id: user2 }, function(err, data, reply) {
+	if (err) {
+	    return handleError(err);
+	}
     });
 }
 
@@ -187,40 +223,43 @@ function postAboutTopic(topic) {
 // Keep track of if we're already processing tweets, so we don't start again
 var processing = false;
 
-var numAdded = 0;
 var stream;
 var startListeningTime = null;
 
-// This keeps track of how our streamin is doing
+// This keeps track of how our streaming is doing
 // It will make decisions on whether we should stop or continue
 // and if it's been going well enough for 5 minutes, it will
 // send a tweet.
 function monitorStreaming() {
-    if (processing == true && startListeningTime != null) {
-	var currentTime = Date.now();
-	var timePassed = currentTime - startListeningTime;
-	console.log("Been listening to " + topic + " for " + timePassed/1000.0 + "s. Added " + numAdded + " tweets");
-	if (timePassed > 60000 && numAdded < 10) {
-	    stream.stop();
-	    processing = false;
-	    console.log("Abandoning topic: " + topic);
-	} else if (timePassed > 60000*5) {
-	    var newStatus = markovTweet();
-	    console.log("\n|||||||||||| Status: " + newStatus);
-	    postTweet(newStatus);
-	    stream.stop();
-	    chooseUsers();
-	    followUsers();
-	    sendConnectTweet(user1, user2);
-	    processing = false;
+    try {
+	if (processing == true && startListeningTime != null) {
+	    var currentTime = Date.now();
+	    var timePassed = currentTime - startListeningTime;
+	    console.log("Been listening to " + topic + " for " + timePassed/1000.0 + "s. Added " + numAdded + " tweets");
+	    if (timePassed > 60000 && numAdded < 10) {
+		stream.stop();
+		processing = false;
+		console.log("Abandoning topic: " + topic);
+	    } else if (timePassed > 60000*10) {
+		var newStatus = markovTweet();
+		console.log("\n|||||||||||| Status: " + newStatus);
+		postTweet(newStatus);
+		stream.stop();
+		chooseUsers();
+		//followUsers();
+		//sendConnectTweet(user1, user2);
+		processing = false;
+	    }
+	} else {
+	    console.log("Not currently streaming\n");
 	}
-    } else {
-	console.log("Not currently streaming\n");
+    } catch(e) {
+	console.log("Error in monitor streaming() " + e);
     }
 }
 
 
-connections = [" do you know ", " you know ", " what's up with ", " who is ", " you seem kind of like "] 
+connections = [" do you know ", " you know ", " what's up with ", " who is ", " what do you think about ", " who's ", " I'd like to introduce you to "] 
 function sendConnectTweet(user1, user2) {
     connectionStatus = "@" + user1 + choice(connections) + "@" + user2 + "?"; 
     console.log("\n||||||||||| ConnectionStatus: " + connectionStatus);
@@ -230,48 +269,56 @@ function sendConnectTweet(user1, user2) {
 // This sets up our streaming. Picks a trend in the US, listens to a bunch of tweets about it,
 // and adds the tweets to our corpus
 function connectorStream() {
-    if (processing == true ) {
-	console.log("Still processing...");
-	return;
-    }
-    startListeningTime = null;
-    processing = true;
-    
-    console.log("\n");
-    console.log("\n");
-
-    resetCorpus();
-    
-    console.log("Getting trends");
-    T.get('trends/place', {id: '23424977'}, function(err, reply) {
-	if (err != null) {
-	    console.log(err);
+    try {
+	if (processing == true ) {
+	    console.log("Still processing...");
+	    return;
 	}
+	startListeningTime = null;
+	processing = true;
 	
-	for (i = 0; i < 10; i++ ) {
-	    console.log("Got trend: " + reply[0].trends[i].name);
-	    trends.push(reply[0].trends[i].name)
-	}
-	console.log("Trends: " + trends);
-	topic = chooseTrend();
-	console.log("Selected trend: " + topic);
+	console.log("\n");
+	console.log("\n");
 
-	postAboutTopic(topic);
+	resetCorpus();
 	
-	console.log("Starting listening");
-	stream = T.stream('statuses/filter', { track: topic })
-	
-	startListeningTime = Date.now();
-	stream.on('tweet', function(tweet) {
-	    if (passesFilter(tweet.text)) {
-		// console.log("Adding tweet " + numAdded + " by user: " + tweet.user.screen_name);
-		// console.log("text: " + tweet.text);
-		addTweet(tweet.text);
-		addUser(tweet.user.screen_name);
-		numAdded++;
+	console.log("Getting trends");
+	T.get('trends/place', {id: '23424977'}, function(err, reply) {
+	    if (err) {
+		return handleError(err);
 	    }
+	    
+	    for (i = 0; i < 10; i++ ) {
+		console.log("Got trend: " + reply[0].trends[i].name);
+		trends.push(reply[0].trends[i].name)
+	    }
+	    console.log("Trends: " + trends);
+	    topic = chooseTrend();
+	    console.log("Selected trend: " + topic);
+
+	    postAboutTopic(topic);
+	    
+	    console.log("Starting listening");
+	    stream = T.stream('statuses/filter', { track: topic })
+	    
+	    startListeningTime = Date.now();
+	    stream.on('tweet', function(tweet) {
+		try {
+		    if (passesFilter(tweet.text)) {
+			// console.log("Adding tweet " + numAdded + " by user: " + tweet.user.screen_name);
+			// console.log("text: " + tweet.text);
+			addTweet(tweet.text);
+			addUser(tweet.user.screen_name);
+			numAdded++;
+		    }
+		} catch(e) {
+		    console.log("Error while streaming; tweet: " + tweet + " e: " + e);
+		}
+	    });
 	});
-    });
+    } catch(e) {
+	console.log("Error in connectorStream, e: " + e);
+    }		   
 }
 
 // Compares two long string integers
@@ -292,12 +339,15 @@ var stringGreaterThan = function(a, b) {
 
 // Keep track of which users we applied to
 repliedToUsers = {};
-replyMessages = [" you both seem p cool", " thought you were both interesting", " :)", " you're cool", " you both seem chill", " lol"]
+replyMessages = [" you both seem p cool", " thought you were both interesting", " :)", " you're cool", " you both seem chill", " lol", " my bad"]
 function postReplyToMention(screenName, replyToId) {
     if (!repliedToUsers.hasOwnProperty(screenName)) {
 	repliedToUsers[screenName] = true;
 	tweet = "@" + screenName + choice(replyMessages);
 	T.post('statuses/update', {status: tweet, in_reply_to_status_id: replyToId}, function(err, data, reply) {
+	    if (err) {
+		return handleError(err);
+	    }
 	});
 	console.log("||||||||| Replied: " + tweet);
     } else {
@@ -309,62 +359,52 @@ function postReplyToMention(screenName, replyToId) {
 var lastMentionIdStr = null;
 var firstMentionPull = true;
 function handleMentions() {
-    console.log("Getting mentions\n");
-    T.get('statuses/mentions_timeline', {}, function(e,r) {
-	for (var i=0; i < r.length; r++) {
-	    if (lastMentionIdStr == null || stringGreaterThan(r[i].id_str, lastMentionIdStr) ) {
-		lastMentionIdStr = r[i].id_str;
-		console.log("Setting last mentionId to " + lastMentionIdStr);
-		if (firstMentionPull == false) {
-		    console.log("\n!!!!!!!!! Got a new mention!");
-		    console.log("Mention from user: " + r[i].user.screen_name);
-		    console.log("Mention text: " + r[i].text);
-		    postReplyToMention(r[i].user.screen_name, r[i].id_str);
-		}
-	    }
-	}
-	firstMentionPull = false;
-    });
-}
-
-
-
-function streamers() {
     try {
-	connectorStream();
-    } catch (e) {
-	console.log(e);
+	console.log("Getting mentions\n");
+	T.get('statuses/mentions_timeline', {}, function(e,r) {
+	    try {
+		for (var i=0; i < r.length; r++) {
+		    if (lastMentionIdStr == null || stringGreaterThan(r[i].id_str, lastMentionIdStr) ) {
+			lastMentionIdStr = r[i].id_str;
+			console.log("Setting last mentionId to " + lastMentionIdStr);
+			if (firstMentionPull == false) {
+			    console.log("\n!!!!!!!!! Got a new mention!");
+			    console.log("Mention from user: " + r[i].user.screen_name);
+			    console.log("Mention text: " + r[i].text);
+			    postReplyToMention(r[i].user.screen_name, r[i].id_str);
+			}
+		    }
+		}
+		firstMentionPull = false;
+	    } catch(e) {
+		console.log("Error in getting mentions callback, e: " + e);
+	    }
+	});
+    } catch(e) {
+	console.log("Error in handleMentions, e: " + e);
     }
 }
 
-// Every 1 minute  try to start streaming
-setInterval(streamers, 60000*1 );
 
 // Every 15 seconds see how we're doing
-setInterval(function() {
-    try {
-	monitorStreaming();
-    } catch (e) {
-	console.log(e);
-    }
-}, 1000 * 15 ); 
+setInterval(monitorStreaming, 1000 * 15); 
+
+// Every 1 minute  try to start streaming
+setInterval(connectorStream, 60000*1);
 
 // Wait 2 seconds after startup and start posting
-setTimeout(streamers, 2000);
-
-function mentions() {
-    try {
-	handleMentions();
-    } catch (e) {
-	console.log(e);
-    }
-}
+setTimeout(connectorStream, 2000);
 
 // Every 10 minutes see if we have new mentions
-setInterval(mentions, 60000 * 10);
+setInterval(handleMentions, 60000 * 10);
 
 // Get our first mentions 5 seconds after we start
-setTimeout(mentions, 5000);
+setTimeout(handleMentions, 5000);
 
 // Do a quick first pass on mentions just to test
-setTimeout(mentions, 60000 * 1);
+setTimeout(handleMentions, 60000 * 1);
+
+function handleError(err) {
+  console.error('response status:', err.statusCode);
+  console.error('data:', err.data);
+}
