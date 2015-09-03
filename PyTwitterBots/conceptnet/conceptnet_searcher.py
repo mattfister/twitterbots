140e04700ff15d1385b664119045cb960b4ce0a0
Matthew Fister <mfister@constantcontact.com>
2015-09-03 08:13:24 -0400

    Add reduced concept net

--- PyTwitterBots/conceptnet/conceptnet_searcher.py
+++ PyTwitterBots/conceptnet/conceptnet_searcher.py
@@ -1,40 +1,69 @@
 import csv
 import pickle
-import sPickle
+import json
 
 files = ['part_00.csv', 'part_01.csv', 'part_02.csv', 'part_03.csv', 'part_04.csv', 'part_05.csv', 'part_06.csv',
          'part_07.csv']
 
-relation_dict = {}
+num_parts = 50
+dictionary_parts = []
+for i in range(num_parts):
+    dictionary_parts.append({})
+
+def string_hash(concept):
+    sum = 0
+    for i in range(len(concept)):
+        sum += i*ord(concept[i])
+
+    return sum % num_parts
+
+def get_part_dict(concept):
+    return dictionary_parts[string_hash(concept)]
+
+def chop_csvs():
+
+    for part in files:
+        print part
+        with open('conceptnet/' + part) as csvfile:
+            datareader = csv.reader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
+
+            for row in datareader:
+
+                # print row[0]
+                triplet = row[0].split(',')
+                # print triplet
+                relation = triplet[0].split('/')[4]
+                concept_array = triplet[1].split('/')
+                # print concept_array
+                concept = triplet[1].split('/')[3]
+                concept_locale = triplet[1].split('/')[2]
+                object = triplet[2].split('/')[3]
+                object_locale = triplet[2].split('/')[2]
+                
+                if (concept_locale == 'en' and object_locale == 'en'):
+                    #print 'new concept'
+                    #print relation
+                    #print concept
+                    #print object
+                
+                    relation_dict = get_part_dict(concept) 
+
+                    if concept not in relation_dict:
+                        relation_dict[concept] = []
 
 
-for part in files:
-    print part
-    with open('conceptnet/' + part) as csvfile:
-        datareader = csv.reader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
+                    relation_dict[concept].append((relation, object))
 
-        for row in datareader:
+    for i in range(len(dictionary_parts)):
+        with open ('conceptnet_reduced_en_' + str(i) + '.json', 'w') as outfile:
+            json.dump(dictionary_parts[i], outfile)
 
-            # print row[0]
-            triplet = row[0].split(',')
-            # print triplet
-            relation = triplet[0].split('/')[4]
-            concept_array = triplet[1].split('/')
-            # print concept_array
-            concept = triplet[1].split('/')[3]
-            concept_locale = triplet[1].split('/')[2]
-            object = triplet[2].split('/')[3]
-            object_locale = triplet[2].split('/')[2]
 
-            if (concept_locale == 'en' and object_locale == 'en'):
-                #print 'new concept'
-                #print relation
-                #print concept
-                #print object
+def get_concept_relations(concept):
+    with open('conceptnet/conceptnet_reduced_en_' + str(string_hash(concept)) + '.json', 'r') as f:
+        m = json.load(f)
+        print m[concept]
+    
 
-                if concept not in relation_dict:
-                    relation_dict[concept] = []
-
-                relation_dict[concept].append((relation, object))
-
-sPickle.s_dump(relation_dict, open('conceptnet_reduced_en_5_4.p', 'wb'))
+if __name__ == '__main__':
+    get_concept_relations('desk')
